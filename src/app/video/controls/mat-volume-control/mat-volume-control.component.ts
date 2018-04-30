c64a02385429bb51a3ef96760c9eb9961b689c54
nkoehler <nicholas_koehler@hotmail.ca>
2018-04-30 17:13:45 -0400

    Moved volume logic to respective component

--- src/app/video/controls/mat-volume-control/mat-volume-control.component.ts
+++ src/app/video/controls/mat-volume-control/mat-volume-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'mat-volume-control',
@@ -6,20 +6,58 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./mat-volume-control.component.css']
 })
 export class MatVolumeControlComponent {
+  vol: number = 1;
+  mute: boolean = false;
+
+  @Input() video: HTMLVideoElement = null;
+
   @Input() color = "primary";
-  @Input() volume = 1;
-  @Input() muted = false;
+
+  @Input() get volume() { return this.vol; }
+  set volume(value: number) { this.setVolume(value); }
+
   @Output() volumeChanged = new EventEmitter<number>();
-  @Output() mutedChanged = new EventEmitter();
+
+  @Input() get muted() { return this.mute; }
+  set muted(value: boolean) { this.setMuted(value); }
+
+  @Output() mutedChanged = new EventEmitter<boolean>();
 
   constructor() { }
 
-  volumeEvent(event): void {
-    this.volumeChanged.emit(event.value);
+  setVolume(value: number): void {
+    this.vol = value;
+    this.video.volume = this.vol;
+    this.volumeChanged.emit(this.vol);
+
+    if (this.vol > 0)
+      this.setMuted(false);
   }
 
-  mutedEvent(): void {
-    this.mutedChanged.emit();
+  setMuted(value: boolean): void {
+    if (this.mute !== value)
+      this.toggleMuted();
+  }
+
+  toggleMuted(): void {
+    this.mute = !this.mute;
+    this.updateMuted();
+  }
+
+  updateMuted(): void {
+    this.video.muted = this.mute;
+    this.mutedChanged.emit(this.mute);
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  onKeydownHandler(event: KeyboardEvent) {
+    if (event.defaultPrevented) return;
+
+    const key = event.key || event.keyCode;
+
+    if (key === 'm' || key === 77) this.toggleMuted();
+
+    event.preventDefault();
   }
 
 }
