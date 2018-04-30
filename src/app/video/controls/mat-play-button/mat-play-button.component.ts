0c8571442f3e46b4c7cf6fabb71632926889c3c0
nkoehler <nicholas_koehler@hotmail.ca>
2018-04-30 17:19:39 -0400

    Moved play logic to respective component

--- src/app/video/controls/mat-play-button/mat-play-button.component.ts
+++ src/app/video/controls/mat-play-button/mat-play-button.component.ts
@@ -1,18 +1,56 @@
-import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Component, EventEmitter, HostListener, Input, Output, Renderer2, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'mat-play-button',
   templateUrl: './mat-play-button.component.html',
   styleUrls: ['./mat-play-button.component.css']
 })
-export class MatPlayButtonComponent {
-  @Input() playing = false;
-  @Output() playbackChanged = new EventEmitter();
+export class MatPlayButtonComponent implements AfterViewInit, OnDestroy {
+  playing: boolean = false;
 
-  constructor() { }
+  @Input() video: HTMLVideoElement = null;
+
+  @Input() get play() { return this.playing; }
+  set play(value: boolean) { this.setVideoPlayback(value); }
+
+  @Output() playChanged = new EventEmitter<boolean>();
+
+  clickEvent: () => void;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit(): void {
+    this.clickEvent = this.renderer.listen(this.video, 'click', () => this.toggleVideoPlayback());
+  }
+
+  ngOnDestroy(): void {
+    if (this.clickEvent) this.clickEvent();
+  }
+
+  setVideoPlayback(value: boolean) {
+    if (this.playing !== value)
+      this.toggleVideoPlayback();
+  }
 
   toggleVideoPlayback(): void {
-    this.playbackChanged.emit();
+    this.playing = !this.playing;
+    this.updateVideoPlayback();
+  }
+
+  updateVideoPlayback(): void {
+    this.playing ? this.video.play() : this.video.pause();
+    this.playChanged.emit(this.playing);
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  onKeydownHandler(event: KeyboardEvent) {
+    if (event.defaultPrevented) return;
+
+    const key = event.key || event.keyCode;
+
+    if (key === ' ' || key === 32) this.toggleVideoPlayback();
+
+    event.preventDefault();
   }
 
 }
